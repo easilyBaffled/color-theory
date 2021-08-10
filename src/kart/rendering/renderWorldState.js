5ea00a5e5c51b2d1945ac6b333057b1aa13bf24f
Danny Michaelis <dmichaelis0@gmail.com>
2021-08-10 18:27:57 -0400

    update render to show individual bodies

--- src/kart/rendering/renderWorldState.js
+++ src/kart/rendering/renderWorldState.js
@@ -1,26 +1,31 @@
-const renderScale = 1;
-const renderSegment = ( seg ) =>
-    `background:${seg.color};padding: ${renderScale * 5}px ${
-        renderScale * 7
-    }px;`; // `background:${color};padding: 5px 7px;` // full box
+import { renderSegment } from "./renderSegment";
+import { renderScale } from "./constants";
+
 const renderBody = ( bodies ) =>
     `border: ${renderScale * 3}px dashed; border-color: ${bodies
         .map( ( b ) => b.color )
         .join( " " )};border-radius: 3px`;
 
+export const playerTrackComponent = ( player ) => {
+    const arr = Array( player.pos.val ).fill( 0 );
+    const targets = arr.reduce( ( s ) => s + "%c ", "" ) + "%c ";
+    const styles = arr
+        .map( () => renderSegment({ color: "transparent" }) )
+        .concat( renderSegment( player ) );
+
+    return [ targets, styles ];
+};
+
 export const renderWorldState = ( world ) => {
     const track = world.segments.reduce( ( s ) => s + "%c ", "" ); // can't use join because for the css to work there has to be a space after each '%c' and `.join(' ')` trims the last space
-    const colors = world.segments.map( ( s, i ) => {
-        let css = renderSegment( s );
-        const bodies = world.bodies.filter( ( b ) => b.pos.val === i );
-        if ( bodies.length ) css += renderBody( bodies );
+    const colors = world.segments.map( renderSegment );
 
-        return css;
+    console.log( track, ...colors );
+    world.bodies.forEach( ( b ) => {
+        const [ targets, styles ] = playerTrackComponent( b );
+        styles[ styles.length - 1 ] += "border-radius: 3px";
+        console.log( targets, ...styles );
     });
-    console.log(
-        track,
-        ...colors
-        // ...world.bodies.map( pick([ "pos", "moves", "id" ]) )
-    );
-    console.log( world );
+
+    console.log( "\n", world );
 };
